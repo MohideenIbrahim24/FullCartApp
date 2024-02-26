443af7b2293fb03871d9fb223f66399989c602df
MohideenIbrahim24 <farukthalha23@gmail.com>
2024-02-26 22:56:50 +0530

    Auto-generated commit

--- common.c7795d0c602d6dbf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkFullCart_UI=self.webpackChunkFullCart_UI||[]).push([[592],{4282:(t,e,l)=>{l.d(e,{T:()=>s});const s={production:!1,apiUrl:"https://localhost:5001/api/"}}}]);
